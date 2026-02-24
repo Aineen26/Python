65023b4ff79e79158d360615ef2b34475e36ed21
Aineen26 <aineensayyed2005@gmail.com>
2026-02-24 23:43:49 +0530

    python practice

---
+++ CHAPTER 2/datatypes.py
@@ -0,0 +1,5 @@
+a = 1 # is a integer
+b = 2.4 # is a floating point number
+c = "hello" #is a string
+d = False # is a boolean variable
+e = None #is a none type variable
---
+++ CHAPTER 2/variable.py
@@ -0,0 +1,4 @@
+a = 1
+b = 2
+c = "hello"
+print(a+b)
